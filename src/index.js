330f1bb15c346bfe11f466b8c9bf65ce3feb886f
Matt Sherman <matt@jam123.com>
2018-10-03 14:44:12 -0400

    Reduced number of categories and items in template

--- src/index.js
+++ src/index.js
@@ -37,7 +37,6 @@ registerBlockType( "gm18-gutenberg-price-list/price-list", {
                 <InnerBlocks template={[
                     [ 'gm18-gutenberg-price-list/price-list-category' ],
                     [ 'gm18-gutenberg-price-list/price-list-category' ],
-                    [ 'gm18-gutenberg-price-list/price-list-category' ],
                 ]} />
             </section>
         );
